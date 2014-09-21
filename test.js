7ca008eb1767bc87a0e27ebb08947746b5a2fb64
Søren Rasmussen <brokzftw@gmail.com>
2014-09-21 15:39:26 +0200

    Added tests

---
+++ test.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var assert = require('assert');
+var copypaste = require("copy-paste");
+
+var jlint = require('./');
+
+var fixtures = {
+  parses: '{"hello": 1, "foo": "bar"}',
+  fails: '{"hello": 1, "foo": "bar"}"'
+};
+
+describe('jlint', function(){
+
+  describe('parses', function () {
+    before(function (done) {
+      copypaste.copy(fixtures.parses, function (copy) {
+        copypaste.paste(function (err, content) {
+          done();
+        });
+      });
+    });
+
+    it('should parse', function(done){
+      jlint(function (lint) {
+        assert.equal(lint.parsed, true);
+        assert.equal(lint.content, JSON.stringify(JSON.parse(fixtures.parses), null, 2));
+        assert.equal(lint.exception, void 0);
+
+        done();
+      });
+    });
+  });
+
+  describe('fails', function () {
+    before(function (done) {
+      copypaste.copy(fixtures.fails, function (copy) {
+        copypaste.paste(function (err, content) {
+          done();
+        });
+      });
+    });
+
+    it('should not parse', function(done){
+      jlint(function (lint) {
+        assert.equal(lint.parsed, false);
+        assert.equal(lint.content, fixtures.fails);
+        assert.notEqual(lint.exception, void 0);
+
+        done();
+      });
+    });
+  });
+});
